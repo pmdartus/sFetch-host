5beb0c61e1d8d932f0b3122dd279c297c7fc7412
Kim CHOUARD <kim.chouard@gmail.com>
2013-11-19 00:00:39 -0800

    Switch to new architecture

--- public/js/app.js
+++ public/js/app.js
@@ -2,25 +2,105 @@
 
 // Declare app level module which depends on filters, and services
 
-angular.module('myApp', [
-  'myApp.controllers',
-  'myApp.filters',
-  'myApp.services',
-  'myApp.directives'
+angular.module('SalesFetchApp', [
+  'SalesFetchApp.controllers',
+  'SalesFetchApp.filters',
+  'SalesFetchApp.services',
+  'SalesFetchApp.directives'
 ]).
 config(function ($routeProvider, $locationProvider) {
   $routeProvider.
-    when('/view1', {
+    when('/', {
       templateUrl: 'partials/partial1',
-      controller: 'MyCtrl1'
-    }).
-    when('/view2', {
-      templateUrl: 'partials/partial2',
-      controller: 'MyCtrl2'
+      controller: 'UsersCtrl'
     }).
     otherwise({
       redirectTo: '/view1'
     });
 
   $locationProvider.html5Mode(true);
+}).
+factory('Base64', function() {
+    var keyStr = 'ABCDEFGHIJKLMNOP' +
+            'QRSTUVWXYZabcdef' +
+            'ghijklmnopqrstuv' +
+            'wxyz0123456789+/' +
+            '=';
+    return {
+        encode: function (input) {
+            var output = "";
+            var chr1, chr2, chr3 = "";
+            var enc1, enc2, enc3, enc4 = "";
+            var i = 0;
+
+            do {
+                chr1 = input.charCodeAt(i++);
+                chr2 = input.charCodeAt(i++);
+                chr3 = input.charCodeAt(i++);
+
+                enc1 = chr1 >> 2;
+                enc2 = ((chr1 & 3) << 4) | (chr2 >> 4);
+                enc3 = ((chr2 & 15) << 2) | (chr3 >> 6);
+                enc4 = chr3 & 63;
+
+                if (isNaN(chr2)) {
+                    enc3 = enc4 = 64;
+                } else if (isNaN(chr3)) {
+                    enc4 = 64;
+                }
+
+                output = output +
+                        keyStr.charAt(enc1) +
+                        keyStr.charAt(enc2) +
+                        keyStr.charAt(enc3) +
+                        keyStr.charAt(enc4);
+                chr1 = chr2 = chr3 = "";
+                enc1 = enc2 = enc3 = enc4 = "";
+            } while (i < input.length);
+
+            return output;
+        },
+
+        decode: function (input) {
+            var output = "";
+            var chr1, chr2, chr3 = "";
+            var enc1, enc2, enc3, enc4 = "";
+            var i = 0;
+
+            // remove all characters that are not A-Z, a-z, 0-9, +, /, or =
+            var base64test = /[^A-Za-z0-9\+\/\=]/g;
+            if (base64test.exec(input)) {
+                console.info("There were invalid base64 characters in the input text.\n" +
+                        "Valid base64 characters are A-Z, a-z, 0-9, '+', '/',and '='\n" +
+                        "Expect errors in decoding.");
+            }
+            input = input.replace(/[^A-Za-z0-9\+\/\=]/g, "");
+
+            do {
+                enc1 = keyStr.indexOf(input.charAt(i++));
+                enc2 = keyStr.indexOf(input.charAt(i++));
+                enc3 = keyStr.indexOf(input.charAt(i++));
+                enc4 = keyStr.indexOf(input.charAt(i++));
+
+                chr1 = (enc1 << 2) | (enc2 >> 4);
+                chr2 = ((enc2 & 15) << 4) | (enc3 >> 2);
+                chr3 = ((enc3 & 3) << 6) | enc4;
+
+                output = output + String.fromCharCode(chr1);
+
+                if (enc3 != 64) {
+                    output = output + String.fromCharCode(chr2);
+                }
+                if (enc4 != 64) {
+                    output = output + String.fromCharCode(chr3);
+                }
+
+                chr1 = chr2 = chr3 = "";
+                enc1 = enc2 = enc3 = enc4 = "";
+
+            } while (i < input.length);
+
+            return output;
+        }
+    };
 });
--- public/js/controllers.js
+++ public/js/controllers.js
@@ -2,9 +2,8 @@
 
 /* Controllers */
 
-angular.module('myApp.controllers', []).
+angular.module('SalesFetchApp.controllers', []).
   controller('AppCtrl', function ($scope, $http) {
-
     $http({
       method: 'GET',
       url: '/api/name'
@@ -16,12 +15,37 @@ angular.module('myApp.controllers', []).
       $scope.name = 'Error!'
     });
 
-  }).
-  controller('MyCtrl1', function ($scope) {
-    // write Ctrl here
-
-  }).
-  controller('MyCtrl2', function ($scope) {
-    // write Ctrl here
-
-  });
+  }).controller('UsersCtrl', function($scope, $http, $filter, Base64) {
+        
+        $scope.Date = Date;
+        $scope.loading = false;
+        
+        $scope.getTimeline = function() {
+            $scope.loading = true;
+            $http.defaults.headers.common['Authorization'] = 'Basic ' + Base64.encode('salesfetch@gmail.com' + ':' + 'Dreamforce2013');
+        $http({method: 'GET', url: 'http://api.anyfetch.com/documents?search=Einstein&limit=50'})
+            .success(function(data, status, headers, config) {
+                for (var i = 0; i < data.datas.length; i++) {
+                    var actItem = data.datas[i];
+                    actItem.date = new Date(actItem.creation_date);
+                    
+                    if (actItem.semantic_document_type == "5252ce4ce4cfcd16f55cfa3a") {
+                        console.log("Getting rid of contact");
+                        data.datas.splice(i, 1);
+                        i--;
+                    }
+                }
+                
+                console.log(data.datas);
+                $scope.items = data.datas;
+                $scope.loading = false;
+                // this callback will be called asynchronously
+                // when the response is available
+            })
+            .error(function(data, status, headers, config) {
+                console.log("Error", data);
+                // called asynchronously if an error occurs
+                // or server returns response with an error status.
+            });
+        };
+    });
