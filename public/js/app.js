214332fad3cd4334b76236c4d7c594afeb7e730f
Pierre-Marie Dartus <dartus.pierremarie@gmail.com>
2013-11-18 23:49:23 -0800

    Reset the repo

--- public/js/app.js
+++ public/js/app.js
@@ -1,27 +1,26 @@
 'use strict';
 
 // Declare app level module which depends on filters, and services
-angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives']).
-  config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
-    $routeProvider.
-      when('/', {
-        templateUrl: 'partials/index',
-        controller: IndexCtrl
-      }).
-      when('/contacts', {
-        templateUrl: 'partials/contacts',
-        controller: SearchContact
-      }).
-      when('/contacts/:id', {
-        templateUrl: 'partials/timeline',
-        controller: TimeLine
-      }).
-      when('/document/:id', {
-        templateUrl: 'partials/fullDocuent',
-        controller: DisplayDocument
-      }).
-      otherwise({
-        redirectTo: '/'
-      });
-    $locationProvider.html5Mode(true);
-  }]);
+
+angular.module('myApp', [
+  'myApp.controllers',
+  'myApp.filters',
+  'myApp.services',
+  'myApp.directives'
+]).
+config(function ($routeProvider, $locationProvider) {
+  $routeProvider.
+    when('/view1', {
+      templateUrl: 'partials/partial1',
+      controller: 'MyCtrl1'
+    }).
+    when('/view2', {
+      templateUrl: 'partials/partial2',
+      controller: 'MyCtrl2'
+    }).
+    otherwise({
+      redirectTo: '/view1'
+    });
+
+  $locationProvider.html5Mode(true);
+});
