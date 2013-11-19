ec975f2073488f6c4934d388219faeeb3d3c8326
Kim CHOUARD <kim.chouard@gmail.com>
2013-11-19 14:01:15 -0800

    Loading du père fourra

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -13,9 +13,10 @@ angular.module('SalesFetchApp.controllers', []).
         $scope.Date = Date;
         
         $scope.loading = true;
-        // var queryUrl = "http://api.anyfetch.com/documents?search='+$scope.contact+'&limit=50'";
-        var queryUrl = "/offline_einstein.json";
-        //    $http.defaults.headers.common['Authorization'] = 'Basic ' + Base64.encode('salesfetch@gmail.com' + ':' + 'Dreamforce2013');
+        var queryUrl = 'http://api.anyfetch.com/documents?search='+$scope.contact+'&limit=50';
+        // DEBUG
+        // var queryUrl = "/offline_einstein.json";
+        $http.defaults.headers.common['Authorization'] = 'Basic ' + Base64.encode('salesfetch@gmail.com' + ':' + 'Dreamforce2013');
         $http({method: 'GET', url: queryUrl})
           .success(function(data, status, headers, config) {
             for (var i = 0; i < data.datas.length; i++) {
